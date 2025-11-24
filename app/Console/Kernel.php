8aeabe8ec79a78638d4b2e7b600bbd8573e4292d
Roman Bestfator <r.bestfator@swiftx.ru>
2025-11-24 23:02:39 +0300

    changes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -70,6 +70,8 @@ class Kernel extends ConsoleKernel
             ->dailyAt('02:30')
             ->sendOutputTo(storage_path("{$commonPath}import-sendsay-stats-deliv.log"));
 
+        //todo: при первом запуске - "--createTempTableAndFill" (убрать при следующем запуске команды!!!)
+        //todo: если обновили файл - "--fillTempTable", после заполнения таблицы, убрать при следующем запуске команды!!!
        $schedule->command('calculate:pdd_specialty_common_db --only=verification_status --createTempTableAndFill')
             ->dailyAt('03:30')
             ->sendOutputTo(storage_path("{$commonPath}calculate-pdd-specialty-common-db.log"));
